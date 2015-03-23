39d5242100a6f2d6dfb479ef3a4dfc6048e7dd18
m00nlight <dot_wangyushi@yeah.net>
2015-03-23 09:10:00 +0000

    Modify Stock Prediction Program

--- ai/Machine-Learning/Stock-Predictions/main.py
+++ ai/Machine-Learning/Stock-Predictions/main.py
@@ -1,12 +1,14 @@
 from __future__ import division
 from math import sqrt
+from operator import add
+from heapq import heappush, heappop
 
-def printTransactions(m, k, d, name, owned, prices):
-    def avg(nums):
+def printTransactions(money, k, d, name, owned, prices):
+    def mean(nums):
         return sum(nums) / len(nums)
 
     def sd(nums):
-        average = avg(nums)
+        average = mean(nums)
         return sqrt(sum([(x - average) ** 2 for x in nums]) / len(nums))
 
     def info(price):
@@ -14,12 +16,34 @@ def printTransactions(m, k, d, name, owned, prices):
         for i in range(1, 5): 
             if price[i] > price[i - 1]: cc += 1
         sigma = sd(price)
-        acc = price[-1] - price[0]
-        return (cc, sigma, price[-1], acc)
+        mu = mean(price)
+        c1, c2, c3 = mean(price[0:3]), mean(price[1:4]), mean(price[2:5])
+        
+        return (c1, c2, c3, (price[-1]- price[0]) / price[0])
 
     
     infos = map(info, prices)
-    print infos
+    res = []
+    
+    up = filter(lambda x: x[0] < x[1] and x[1] < x[2] and x[-1] > 0, infos)
+    down = filter(lambda x: x[0] > x[1] and x[1] > x[2] and x[-1] < 0, infos)
+    
+    up_sum = sum(map(lambda x: x[-1], up))
+    down_sum = sum(map(lambda x: x[-1], down))
+    
+    for i in range(k):
+        c1, c2, c3, rate = info(prices[i])
+        if c2 >= c1 and c3 >= c2 and owned[i] > 0:
+            res.append((name[i], 'SELL', str(owned[i])))
+        elif c2 <= c1 and c3 <= c2:
+            amount = int(money * rate / down_sum / prices[i][-1])
+            if amount > 0: res.append((name[i], 'BUY', str(amount)))
+    
+    print len(res)
+    for r in res:
+        print ' '.join(r)
+    
+    
 
 if __name__ == '__main__':
     m, k, d = [float(i) for i in raw_input().strip().split()]
