275374f0a3d202f8e1978eed70083fe57415a131
m00nlight <dot_wangyushi@yeah.net>
2015-04-17 22:08:53 +0800

    Accept Special Multipler in Number Theory

--- algorithm/Number-Theory/Special-Multiplier/main.py
+++ algorithm/Number-Theory/Special-Multiplier/main.py
@@ -1,16 +1,13 @@
-# Not accepted yet
-
 from __future__ import division
 
-def solve(num):
-    mul = 1
-    while True:
-        if set(str(mul * num)) == {'0', '9'}: return mul * num
-        mul += 1
-    
-if __name__ == '__main__':
-    n = int(raw_input())
-    for _ in range(n):
-        num = int(raw_input())
-        print solve(num)
+def solve(n):
+	j = 1
+	while (int(str(bin(j))[2:].replace('1', '9')) % n != 0):
+		j += 1
+	return str(bin(j))[2:].replace('1', '9')
 
+if __name__ == '__main__':
+	t = input()
+	for _ in range(t):
+		n = input()
+		print solve(n)
