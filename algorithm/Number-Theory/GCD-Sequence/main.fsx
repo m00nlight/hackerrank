2285926f40aeb41cc139f7c3c2ef67d0c69a2fd2
m00nlight <dot_wangyushi@yeah.net>
2015-09-30 20:46:55 +0800

    Refine GCD Sequence Fsharp code

--- algorithm/Number-Theory/GCD-Sequence/main.fsx
+++ algorithm/Number-Theory/GCD-Sequence/main.fsx
@@ -10,14 +10,11 @@ let calcMobius upBound =
       Array.set ret j (ret.[j] - ret.[i])
   ret
 
-let rec powMod (a : int64) (b : int64) (c : int64) =
-  if b = 0L then 
-    1L
-  else
-    let tmp = powMod a (b / 2L) c
-    if b % 2L = 0L then 
-      tmp * tmp % c
-    else tmp * tmp % c * a % c
+let rec powMod (a : int64) (b : int64) (c : int64) (acc : int64) =
+  if b = 0L then acc
+  elif b % 2L = 0L then
+    powMod (a * a % c) (b / 2L) c acc
+  else powMod (a * a % c) (b / 2L) c (a * acc % c)
 
 let calcFactMod upBound = 
   let ret : int64 array = Array.zeroCreate (upBound + 1)
@@ -31,21 +28,19 @@ let fact = calcFactMod 200010
 
 let func n r = 
   let ret = fact.[n + r - 1]
-  let ret = ret * powMod fact.[r] (mmod - 2L) mmod % mmod
-  let ret = ret * powMod fact.[n - 1] (mmod - 2L) mmod % mmod
+  let ret = ret * powMod fact.[r] (mmod - 2L) mmod 1L % mmod
+  let ret = ret * powMod fact.[n - 1] (mmod - 2L) mmod 1L % mmod
   ret
   
 let _ = 
-    let testCases = System.Int32.Parse(System.Console.ReadLine()) 
-    for t in 1..testCases do
-      let nk = System.Console.ReadLine().Trim().Split([|' '|]) 
-               |> Seq.toList 
-               |> List.map (fun x -> System.Int32.Parse(x))
-      let n = nk.[0]
-      let k = nk.[1]
-      let ans = List.fold
-                  (fun acc i -> 
-                    (acc + mmod + (func (n / i) k) * mu.[i]) % mmod)
-                  0L 
-                  [1..n]
-      printfn "%d" ans
+  let testCases = System.Int32.Parse(System.Console.ReadLine()) 
+  for t in 1..testCases do
+    let nk = System.Console.ReadLine().Trim().Split([|' '|]) 
+              |> Seq.toList 
+              |> List.map (fun x -> System.Int32.Parse(x))
+    let n = nk.[0]
+    let k = nk.[1]
+    let ans = [1..n]
+              |> Seq.fold (fun acc i -> 
+                  (acc + mmod + (func (n / i) k) * mu.[i]) % mmod) 0L
+    printfn "%d" ans
